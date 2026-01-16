8f6b999fb494df701a440bae703e3918c691913e
hetari <hetari4py@gmail.com>
2026-01-16 19:16:26 +0300

    Add nuxt-booster and image config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,7 @@ export default defineNuxtConfig({
     "@nuxtjs/sitemap",
     "nuxt-seo-utils",
     "nuxt-link-checker",
+    "nuxt-booster",
   ],
 
   colorMode: {
@@ -50,5 +51,59 @@ export default defineNuxtConfig({
   site: {
     url: "https://mesh-magic.netlify.app/",
     name: "Mesh Magic",
+  },
+
+  // nuxt booster:
+
+  booster: {
+    detection: {
+      performance: true,
+      browserSupport: true,
+    },
+
+    performanceMetrics: {
+      device: {
+        hardwareConcurrency: { min: 2, max: 48 },
+        deviceMemory: { min: 2 },
+      },
+      timing: {
+        fcp: 800,
+        dcl: 1200,
+      },
+    },
+
+    targetFormats: ["webp", "avif", "jpg|jpeg|png|gif"],
+
+    componentAutoImport: false,
+    componentPrefix: undefined,
+
+    /**
+     * IntersectionObserver rootMargin for Compoennts and Assets
+     */
+    lazyOffset: {
+      component: "0%",
+      asset: "0%",
+    },
+  },
+
+  image: {
+    screens: {
+      default: 320,
+      xxs: 480,
+      xs: 576,
+      sm: 768,
+      md: 996,
+      lg: 1200,
+      xl: 1367,
+      xxl: 1600,
+      "4k": 1921,
+    },
+
+    domains: ["img.youtube.com", "i.vimeocdn.com"],
+
+    alias: {
+      youtube: "https://img.youtube.com",
+      vimeo: "https://i.vimeocdn.com",
+    },
   },
 });
